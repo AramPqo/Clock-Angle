5bd500eaacc9d6efb3e1e4f03582b404fb91ec5f
Aram33 <Aram33@bitbucket.org>
2019-05-17 00:17:26 +0400

    'cm'

--- logic.js
+++ logic.js
@@ -1,12 +1,13 @@
 const h = document.getElementById('hour');
 const m = document.getElementById('minute');
 const text = document.getElementById('text');
-const minHand = document.getElementById('min-hand');
+const minuteHand = document.getElementById('min-hand');
 const hourHand = document.getElementById('hour-hand');
 const dial = document.getElementById('dial');
 const submit = document.getElementById('submit');
 let result;
 let x = 0;
+
 for (let i = 0; i < 60; i++) {
     x += 6;
     const span = document.createElement("SPAN");
@@ -20,7 +21,7 @@ function computeAngle() {
     const minute = parseInt(m.value);
     const minAngle = minute * 6;
     const hourAngle = hour * 30 + minute * (360 / 720);
-    minHand.style.transform = `rotate(${minAngle}deg)`;
+    minuteHand.style.transform = `rotate(${minAngle}deg)`;
     hourHand.style.transform = `rotate(${hourAngle}deg)`;
     result = ((hour + minute / 60) * 30) - minute * 6;
     if (isNaN(Math.abs(result))) {
